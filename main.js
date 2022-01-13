c7d2f6697127b59546e83ce20eec81b710da4765
Alessio-Tomei <alessiotomei22@gmail.com>
2022-01-13 18:18:25 +0100

    Consegna Finita

---
+++ main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        h1Message: 'Ma te vuoi fare la baldoria!',
+        urlImg: 'https://www.tiktok.com/api/img/?itemId=7040051355574144262&location=0&aid=1988',
+    }
+});
